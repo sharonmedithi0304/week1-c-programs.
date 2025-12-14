10e5f73d1057afb84eb2e44a8aaca05936a87259
sharonmedithi0304 <sharonmedithi23@gmail.com>
2025-12-14 21:39:24 +0530

    Add files via upload

---
+++ diff.of 2 num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, diff;
+
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &num1, &num2);
+
+    diff = num1 - num2;
+
+    printf("Difference = %d\n", diff);
+
+    return 0;
+}
+
